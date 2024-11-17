31f8a820973e844966a4f9e574a0e0a1e05ff0a9
CRX DEV <93229466+cyrax-dev@users.noreply.github.com>
2024-11-17 18:16:06 +0300

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,26 @@
+@echo off
+color 0A
+chcp 65001 > nul
+
+echo Creating venv...
+echo ---------------------------------
+python -m venv .venv
+cls
+
+echo Activation venv...
+echo ---------------------------------
+call .venv\Scripts\activate.bat
+cls
+
+echo Updating pip
+echo ---------------------------------
+python.exe -m pip install --upgrade pip
+cls
+
+echo Updating Libs...
+echo ---------------------------------
+pip install -r requirements.txt
+cls
+
+python -B main.py
+pause
